21d51dc464bc86c261bc87f648d1330cd4358c57
suellenferraz <108441500+suellenferraz@users.noreply.github.com>
2025-04-03 21:56:25 +0100

    Day 1

---
+++ Closures/2667.js
@@ -0,0 +1,18 @@
+// Create Hello World Function
+
+/**
+ * @return {Function}
+ */
+var createHelloWorld = function() {
+    
+    return () => "Hello World"
+};
+
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
+
+// You don't have to do all that for Hello World:
+
+console.log("Hello World")
