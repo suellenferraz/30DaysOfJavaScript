15d759920e5f0f197461d8e9066031e932e5363f
suellenferraz <108441500+suellenferraz@users.noreply.github.com>
2025-04-20 22:53:31 +0100

    Day 18

---
+++ Promises and Time/2927.js
@@ -0,0 +1,21 @@
+// Debouce - Day 18
+
+/**
+ * @param {Function} fn
+ * @param {number} t milliseconds
+ * @return {Function}
+ */
+const debounce = function(fn, t = 1000) {
+    let timer;
+    return function(...args) {
+        clearTimeout(timer);
+        timer = setTimeout(() => fn(...args),t);
+    }
+};
+
+/**
+ * const log = debounce(console.log, 100);
+ * log('Hello'); // cancelled
+ * log('Hello'); // cancelled
+ * log('Hello'); // Logged at t=100ms
+ */
